1e2cf6d189cbd67452ff095a95eaf84c869c6e64
Devollox.fun <122895078+Devollox@users.noreply.github.com>
2023-10-20 20:19:14 +0300

    Add files via upload

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -13,9 +13,9 @@ const Modal = ({ open, handleClick }) => {
                 <div className="contentNew" id="getHideContent">
                     <div>
                         <ul className="table">
-                            <li onClick={handlerClickSteam} id="steamOfLi"><a className="lineHideBlock">Steam</a></li>
-                            <li onClick={handlerClickGithub} id="githubOfLi"><a className="lineHideBlock">GitHub</a></li>
-                            <li id="otherOfLi"><a className="lineHideBlock">Other</a></li>
+                            <li><a href='https://steamcommunity.com/id/Cookiemp4/' className="btn-modal lineHideBlock">Steam</a></li>
+                            <li id="githubOfLi"><a href='https://github.com/Devollox' className="btn-modal lineHideBlock">GitHub</a></li>
+                            <li id="otherOfLi"><a className="btn-modal lineHideBlock">Other</a></li>
                         </ul>
                     </div>
                 </div>
