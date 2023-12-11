f340ec47f39af4d392c7983d03f50a3f10faea38
Devollo <122895078+Devollox@users.noreply.github.com>
2023-12-11 06:11:10 +0300

    Delete src directory

--- src/hook/handlerClickIsToogle.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-
-export default function handlerClickIsToogle() {
-    let getClickNavbar : any = document.getElementById('getClickNavbar')
-    getClickNavbar.classList.toggle("change");
-}
--- src/hook/sliteContent.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-export default function useSlite() {
-    let box : any = document.getElementById('box')
-    box.scrollIntoView({block: "center", behavior: "smooth"});
-}
