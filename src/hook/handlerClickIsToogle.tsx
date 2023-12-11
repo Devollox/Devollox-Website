d56a5c1e823c1195058c1aa42590768bc5ba9a91
Devollo <122895078+Devollox@users.noreply.github.com>
2023-12-11 03:46:35 +0300

    Add files via upload

---
+++ src/hook/handlerClickIsToogle.tsx
@@ -0,0 +1,6 @@
+'use client'
+
+export default function handlerClickIsToogle() {
+    // @ts-ignore
+    document.getElementById('getClickNavbar').classList.toggle("change");
+}
---
+++ src/hook/sliteContent.tsx
@@ -0,0 +1,5 @@
+
+'use client'
+export default function useSlite() {
+    document.getElementById("box").scrollIntoView({block: "center", behavior: "smooth"});
+}
