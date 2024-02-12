21d8fe77562077a0840308a8aa195c2854f40e03
Devollo <122895078+Devollox@users.noreply.github.com>
2024-02-12 19:59:10 +0300

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import axios from "axios";
 import {BrowserRouter as Router, Switch, Route, Link, Routes, BrowserRouter} from 'react-router-dom';
-import Update from "./Update/update";
 export default function Footer() {
 
     const WrapperContent = styled.div`
@@ -24,52 +23,10 @@ export default function Footer() {
         border-color: rgb(var(--tw-border-color-opacity));;
     `
 
-    Promise.all([axios.get("https://api.github.com/repos/Devollox/Devollox/commits")])
-        .then(function (result ){
-            let date  = result[0].data[0].commit.author.date, time = document.getElementById('time'), placeHolder = document.getElementById('place-holder'), year = new Date();
-            let optionsTime = {
-                optionsDay: {
-                    day: 'numeric',
-                },
-                optionsMonth: {
-                    month: 'numeric'
-                }
-            }
-            let getDate = function (str) {
-                let date = new Date(str);
-                return [date.toLocaleString('ru', optionsTime.optionsMonth), date.toLocaleString('ru', optionsTime.optionsDay)]
-            }
-
-            setTimeout(() => {
-                placeHolder.style.display = 'none'
-                time.innerHTML = `
-                <a class="date_container_time">
-                    <a title="Month/Day/Year" style="color: var(--colorfg)" class="hiding_text date_footer">
-                        ${getDate(date)[0]}/
-                        ${getDate(date)[1]}/
-                        ${year.getFullYear()}
-                    </a>
-                 </a>
-            `
-            }, 2000)
-        })
-
     return (
         <WrapperContent>
             <WrapperText style={{display: "block"}}>
                 <a>This site was developed by Devollox and is getting regular updates</a>
-                <div className='time_update'>
-                    <div>Latest update:
-                        <a href='/update' style={{color: "var(--colorfg)"}}>
-                             <a className="post">
-                                <div style={{marginBottom: '10px'}} id="time" className='place-font'>
-                                 </div>
-                                    <div id='place-holder' className="place-holder">
-                                 </div>
-                             </a>
-                        </a>
-                    </div>
-                </div>
             </WrapperText>
         </WrapperContent>
     )
